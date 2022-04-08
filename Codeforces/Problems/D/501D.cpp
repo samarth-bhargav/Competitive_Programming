3933fcd764322b4057a01e9b15bf9bacbac0a952
Samarth <samarthmbhargav@gmail.com>
2022-04-08 10:45:16 -0400

    Solved 501D

--- Codeforces/Problems/D/501D.cpp
+++ Codeforces/Problems/D/501D.cpp
@@ -1,19 +1,60 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 
+using namespace __gnu_pbds;
 using namespace std;
 
-typedef long long ll;
+typedef tree<int, null_type, less<>, rb_tree_tag, tree_order_statistics_node_update> ost;
 
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    ll n,k,s;
-    cin >> n >> k >> s;
-    if (k > s || k*(n-1) < s){
-        cout << "NO\n";
+int main(){
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+    for (int i = 0; i < n; i++){
+        cin >> a[i];
     }
-    else{
-        
+    for (int i = 0; i < n; i++){
+        cin >> b[i];
     }
+    vector<int> ao, bo;
+    ost ah, bh;
+    for (int i = 0; i < n; i++){
+        ah.insert(i);
+        bh.insert(i);
+    }
+    for (int i = 0; i < n; i++){
+        ao.push_back(ah.order_of_key(a[i]));
+        ah.erase(ah.find(a[i]));
+        bo.push_back(bh.order_of_key(b[i]));
+        bh.erase(bh.find(b[i]));
+    }
+    vector <int> so(n);
+    for (int i = 0; i < n; i++){
+        so[i] = ao[i] + bo[i];
+    }
+    for (int i = n-1; i > 0; i--){
+        int lim = (n - i);
+        if (so[i] >= lim){
+            int div = so[i] / lim;
+            int rem = so[i] % lim;
+            so[i] = rem;
+            so[i-1] += div;
+        }
+    }
+    so[0] %= n;
+    vector<int> ans;
+    ost sh;
+    for (int i = 0; i < n; i++){
+        sh.insert(i);
+    }
+    for (int i = 0; i < n; i++){
+        auto it = sh.find_by_order(so[i]);
+        ans.push_back(*it);
+        sh.erase(it);
+    }
+    for (int i = 0; i < n; i++){
+        cout << ans[i] << " ";
+    }
+    cout << "\n";
 }
-
